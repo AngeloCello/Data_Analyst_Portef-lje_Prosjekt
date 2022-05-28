5e9328936f83ced6fd5116f48f5efb026ff33dee
AngeloCello <105160326+AngeloCello@users.noreply.github.com>
2022-05-28 08:43:20 +0200

    Add files via upload

---
+++ SQL Data Rensing Portefølje Prosjekt.sql
@@ -0,0 +1,179 @@
+/*
+Rense Data in SQL
+*/
+
+
+SELECT *
+FROM PortfolioProject.dbo.NashvilleHousing
+
+--------------------------------------------------------------------------------------------------------------------------
+
+-- 1. Standardisering av Dato Format
+
+SELECT SaleDate, CONVERT(Date, SaleDate)
+FROM PortfolioProject.dbo.NashvilleHousing
+
+ALTER TABLE PortfolioProject.dbo.NashvilleHousing
+ADD SaleDateConverted Date
+
+UPDATE PortfolioProject.dbo.NashvilleHousing
+SET SaleDateConverted = CONVERT(Date, SaleDate)
+
+
+
+
+ --------------------------------------------------------------------------------------------------------------------------
+
+-- 2. Populere tomme (Property Address) data
+
+SELECT *
+FROM PortfolioProject.dbo.NashvilleHousing
+WHERE PropertyAddress is null
+ORDER BY ParcelID
+
+
+SELECT a.PropertyAddress, b.PropertyAddress, ISNULL(a.PropertyAddress, b.PropertyAddress)
+FROM PortfolioProject.dbo.NashvilleHousing a
+JOIN PortfolioProject.dbo.NashvilleHousing b
+	ON a.ParcelID = b.ParcelID
+	AND a.[UniqueID ] != b.[UniqueID ]
+WHERE a.PropertyAddress is null
+
+
+UPDATE a
+SET PropertyAddress = ISNULL(a.PropertyAddress, b.PropertyAddress)
+FROM PortfolioProject.dbo.NashvilleHousing a
+JOIN PortfolioProject.dbo.NashvilleHousing b
+	ON a.ParcelID = b.ParcelID
+	AND a.[UniqueID ] != b.[UniqueID ]
+WHERE a.PropertyAddress is null
+
+
+
+
+--------------------------------------------------------------------------------------------------------------------------
+
+-- 3. Dele opp [PropertyAddress] i individuelle kolonner (Address, City)
+
+SELECT *
+FROM PortfolioProject.dbo.NashvilleHousing
+
+
+SELECT PropertyAddress, SUBSTRING(PropertyAddress, 1, CHARINDEX(',', PropertyAddress) -1),
+						SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress) +1, LEN(PropertyAddress))
+FROM PortfolioProject.dbo.NashvilleHousing
+
+
+ALTER TABLE PortfolioProject.dbo.NashvilleHousing
+ADD PropertySplitAddress Nvarchar(255)
+
+ALTER TABLE PortfolioProject.dbo.NashvilleHousing
+ADD PropertySplitCity Nvarchar(255)
+
+UPDATE PortfolioProject.dbo.NashvilleHousing
+SET PropertySplitAddress = SUBSTRING(PropertyAddress, 1, CHARINDEX(',', PropertyAddress) -1)
+
+UPDATE PortfolioProject.dbo.NashvilleHousing
+SET PropertySplitCity = SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress) +1, LEN(PropertyAddress))
+
+
+
+
+--------------------------------------------------------------------------------------------------------------------------
+
+-- 4. Dele opp [OwnerAddress] i individuelle kolonner (Address, City, State)
+
+SELECT OwnerAddress
+FROM PortfolioProject.dbo.NashvilleHousing
+
+SELECT OwnerAddress, PARSENAME(REPLACE(OwnerAddress, ',', '.'), 3),
+					 PARSENAME(REPLACE(OwnerAddress, ',', '.'), 2),
+					 PARSENAME(REPLACE(OwnerAddress, ',', '.'), 1)
+FROM PortfolioProject.dbo.NashvilleHousing
+
+
+ALTER TABLE PortfolioProject.dbo.NashvilleHousing
+ADD OwnerSplitAddress Nvarchar(255)
+
+ALTER TABLE PortfolioProject.dbo.NashvilleHousing
+ADD OwnerSplitCity Nvarchar(255)
+
+ALTER TABLE PortfolioProject.dbo.NashvilleHousing
+ADD OwnerSplitState Nvarchar(255)
+
+
+UPDATE PortfolioProject.dbo.NashvilleHousing
+SET OwnerSplitAddress = PARSENAME(REPLACE(OwnerAddress, ',', '.'), 3)
+
+UPDATE PortfolioProject.dbo.NashvilleHousing
+SET OwnerSplitCity = PARSENAME(REPLACE(OwnerAddress, ',', '.'), 2)
+
+UPDATE PortfolioProject.dbo.NashvilleHousing
+SET OwnerSplitState = PARSENAME(REPLACE(OwnerAddress, ',', '.'), 1)
+
+
+
+
+--------------------------------------------------------------------------------------------------------------------------
+
+-- 5. Endre Y og N til Yes og No i [SoldAsVacant] kolonnet
+
+SELECT DISTINCT(SoldAsVacant), COUNT(SoldAsVacant)
+FROM PortfolioProject.dbo.NashvilleHousing
+GROUP BY SoldAsVacant
+ORDER BY 2
+
+
+SELECT SoldAsVacant
+, CASE When SoldAsVacant = 'Y' Then 'Yes'
+       When SoldAsVacant = 'N' Then 'No'
+	   Else SoldAsVacant
+  END
+FROM PortfolioProject.dbo.NashvilleHousing
+WHERE SoldAsVacant = 'Y'
+OR SoldAsVacant = 'N'
+
+
+UPDATE PortfolioProject.dbo.NashvilleHousing
+SET SoldAsVacant = CASE When SoldAsVacant = 'Y' Then 'Yes'
+						When SoldAsVacant = 'N' Then 'No'
+						Else SoldAsVacant
+				   END
+
+
+
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+-- 6. Fjerne Duplikat Rad
+
+SELECT *
+FROM PortfolioProject.dbo.NashvilleHousing
+
+
+WITH RowNumCTE AS (
+SELECT *, ROW_NUMBER() OVER (Partition by 
+						     ParcelId,
+							 PropertyAddress,
+							 SaleDate, 
+							 SalePrice, 
+							 LegalReference Order By UniqueId) AS row_num
+FROM PortfolioProject.dbo.NashvilleHousing
+)
+DELETE
+FROM RowNumCTE
+WHERE row_num > 1
+
+
+
+
+---------------------------------------------------------------------------------------------------------
+
+-- 7. Slette Ubrukt Kolonner
+
+SELECT *
+FROM PortfolioProject.dbo.NashvilleHousing
+
+
+ALTER TABLE PortfolioProject.dbo.NashvilleHousing
+DROP COLUMN PropertyAddress, SaleDate, OwnerAddress 
